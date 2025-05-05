95e7549777d25d1d7128f20253cb1150177b1f29
fido1hn <confidoekeanya@gmail.com>
2025-05-05 19:53:08 +0100

    method 3 with array helpers

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,19 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// Starting here
+// Starting here - Method 3 using array helper fn reduce
 function reverse(str) {
-  // Method 1 - builtin array reverse fn
-  // return str.split("").reverse().join("");
-
-  // Method 2 - for loop
-  let reversed = "";
-
-  for (let char of str) {
-    reversed = char + reversed;
-  }
-
-  return reversed;
+  return str.split("").reduce((reversed, char) => char + reversed, "");
 }
 
 module.exports = reverse;
+
+// Method 1 - builtin array reverse fn
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Method 2 - For loop
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
