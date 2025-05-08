26d8302dad462176c9f488fb17089566c78f0ecb
fido1hn <confidoekeanya@gmail.com>
2025-05-08 02:39:06 +0100

    maxChar second solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,14 +6,22 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  let count = {};
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
   for (let char of str) {
-    count[char] = count[char] + 1 || 1;
+    charMap[char] = charMap[char] + 1 || 1;
   }
-  let max = Math.max(...Object.values(count));
-  for (let x in count) {
-    if (count[x] === max) return x;
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
   }
+
+  return maxChar;
 }
 
 module.exports = maxChar;
@@ -29,5 +37,27 @@ function maxChar(str) {
   for (let x in count) {
     if (count[x] === max) return x;
   }
+}
+*/
+
+// Method 2 solution
+/*
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let x in charMap) {
+    if (charMap[x] > max) {
+      max = charMap[x];
+      maxChar = x;
+    }
+  }
+
+  return maxChar;
 }
 */
