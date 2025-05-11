f794cf42d16f39d701e2b21f7c58a3a94d5c149e
fido1hn <confidoekeanya@gmail.com>
2025-05-11 01:46:01 +0100

    my first solution for the anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,63 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const normalizeA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const normalizeB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  const countA = {};
+  const countB = {};
+
+  for (let el of normalizeA) {
+    countA[el] = countA[el]++ || 1;
+  }
+
+  for (let el of normalizeB) {
+    countB[el] = countB[el]++ || 1;
+  }
+
+  if (Object.keys(countA).length !== Object.keys(countB).length) {
+    return false;
+  }
+
+  for (let k in countA) {
+    if (countB[k] !== countA[k]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
+
+// My first solution
+/*
+function anagrams(stringA, stringB) {
+  const normalizeA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const normalizeB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  const countA = {};
+  const countB = {};
+
+  for (let el of normalizeA) {
+    countA[el] = countA[el]++ || 1;
+  }
+
+  for (let el of normalizeB) {
+    countB[el] = countB[el]++ || 1;
+  }
+
+  if (Object.keys(countA).length !== Object.keys(countB).length) {
+    return false;
+  }
+
+  for (let k in countA) {
+    if (countB[k] !== countA[k]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+*/
